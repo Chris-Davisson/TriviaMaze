1cf2a51ffbbb2f0bfebbb52aa53929d56937dc32
Morgan <57687407+DaLivelyGhost@users.noreply.github.com>
2020-05-16 15:22:33 -0700

    SQL update
    Need to improve on string splitting, because commas in quotes are breaking up strings where they shouldn't be. Will get to that tonight.

--- QuestionDatabase.java
+++ QuestionDatabase.java
@@ -9,23 +9,29 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class QuestionDatabase {
-    	
+
+      //This is the Middle man for all database interactions. All queries to the database will go through here
+     //because this class optimally will handle exceptions and null pointer exceptions to keep that nonsense from cluttering
+    //up the main code.
+
+
     FileReader fileIn;
     BufferedReader fileReader;
-    Connection dbCon;         
+    Connection dbCon;
     Statement stmt;          //DDL (really only used for database creation
     PreparedStatement dml;  //data management (insert, update, select)
-    
+
     final String DATABASE_FILE = "jdbc:sqlite:QuestionDatabase.db";
+    final String TABLE_DROP = "DROP TABLE questions";
     final String TABLE_CREATE = "CREATE TABLE IF NOT EXISTS questions (\n"
-                            + "Question varchar(255), \n"
-                            + "Answer1 varchar(255), \n"
-                            + "Answer2 varchar(255), \n"
-                            + "Answer3 varchar(255), \n"
-                            + "Answer4 varchar(255), \n"
-                            + "Correct int, \n"
-                            +");";
-    
+            + "Question varchar(255), \n"
+            + "Answer1 varchar(255), \n"
+            + "Answer2 varchar(255), \n"
+            + "Answer3 varchar(255), \n"
+            + "Answer4 varchar(255), \n"
+            + "Correct int \n"
+            +");";
+
     public QuestionDatabase(String fileName) {
         setFile(fileName);
     }
@@ -33,13 +39,13 @@ public class QuestionDatabase {
     //Setters
     //-----------------------------------------------
 
-     //main purpose for this method is so that constructing a QuestionDatabase object
+    //main purpose for this method is so that constructing a QuestionDatabase object
     //doesn't require a try catch block. It's public in case we want to be able to switch files at runtime.
     public void setFile(String fileName) {
 
         try {
             //File fileIn = new File(fileName);
-            this.fileIn = new FileReader(fileName);	
+            this.fileIn = new FileReader(fileName);
         }
         catch(FileNotFoundException e) {
             this.fileIn = null;
@@ -55,34 +61,48 @@ public class QuestionDatabase {
         try {
             this.dml = this.dbCon.prepareStatement("SELECT * from questions");
             ResultSet r = this.dml.executeQuery();
-            
+
             while(r.next()) {
-               System.out.println(r.getString("Question") + "\t" + r.getString("Answer"));
+                System.out.println(r.getString("Question") + "\t" + r.getString("Answer1")
+                                                                      + "\t" + r.getString("Answer2")
+                                                                      + "\t" + r.getString("Answer3")
+                                                                      + "\t" + r.getString("Answer4")
+                                                                      + "\t" + r.getInt("Correct"));
             }
         }
         catch(Exception e) {
             e.getMessage();
             System.out.println(e);
         }
-        
+
     }
 
     //-----------------------------------------------
     //Other Methods
     //-----------------------------------------------
+    public void dropTable() {
 
-      //Called by the setFile. Will convert the .csv file into a sql compatible database.
-     //This is called by setFile instead of the constructor so that if the file for questions is switched at run time
+        try {
+            this.stmt.execute(TABLE_DROP);
+
+        } catch (SQLException throwables) {
+
+            throwables.printStackTrace();
+        }
+
+    }
+
+    //Called by the setFile. Will convert the .csv file into a sql compatible database.
+    //This is called by setFile instead of the constructor so that if the file for questions is switched at run time
     //the questiondatabase object will automatically convert to sql.
     private void convertDatabase(FileReader fileIn) {
-	
-        
-         //This try catch block will build the .db (database) file
+
+
+        //This try catch block will build the .db (database) file
         //and then create a table for the questions/answers.
         try {
-            
             this.dbCon = DriverManager.getConnection(DATABASE_FILE);
-            
+
             if(dbCon != null) {
                 DatabaseMetaData mData = this.dbCon.getMetaData();
                 this.stmt = this.dbCon.createStatement();
@@ -93,17 +113,17 @@ public class QuestionDatabase {
             e.getMessage();
             System.out.println(e);
         }
-	
-        
-     //This try catch block will read the information from the csv file
-    //and then convert to sql.
+
+
+        //This try catch block will read the information from the csv file
+        //and then convert to sql.
         try {
-			
+
             fileReader = new BufferedReader(fileIn);
             String line = "";
-			
+
             while((line = fileReader.readLine()) != null) {
-            
+
                 String[] fileValues = line.split(",");
                 this.dml = this.dbCon.prepareStatement("INSERT INTO questions(Question, Answer1, Answer2, Answer3, Answer4, Correct) VALUES(?, ?, ?, ?, ?, ?)");
                 this.dml.setString(1, fileValues[0]);
@@ -113,11 +133,11 @@ public class QuestionDatabase {
                 this.dml.setString(5, fileValues[4]);
                 this.dml.setInt(6, Integer.parseInt(fileValues[5]));
                 this.dml.executeUpdate();
-            }			
-        }   
+            }
+        }
         catch(Exception e) {
-			
-        }   
-		
+
+        }
+
     }
 }
