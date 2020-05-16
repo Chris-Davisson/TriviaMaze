4b4f8fdde0d13ea7217031a1d72b805a7a1efa19
Morgan <57687407+DaLivelyGhost@users.noreply.github.com>
2020-05-16 15:22:53 -0700

    Delete sqlTester.java

--- sqlTester.java
+++
@@ -1,7 +0,0 @@
-
-public class sqlTester {
-    public static void main(String[] args) {
-        QuestionDatabase database = new QuestionDatabase("C:\\Users\\combs\\Desktop\\Class Work\\CSCD350\\GroupQuizMaze\\GroupQuizMaze\\src\\main\\java\\Questions.csv");
-        database.getTable();
-    }
-}
