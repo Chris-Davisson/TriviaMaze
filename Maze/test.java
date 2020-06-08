3562186360f33ef5519177de17e052d0d246efbc
Christopher Davisson <davissonchris@gmail.com>
2020-06-08 16:00:23 -0700

    Just uploading what I got so far
    it dont work yet

---
+++ Maze/test.java
@@ -0,0 +1,7 @@
+public class test {
+    public static void main(String[] args) {
+//         QuestionDatabase database = new QuestionDatabase("Questions.csv");
+//         database.dropTable();
+        new TriviaGUI();
+    }
+}
