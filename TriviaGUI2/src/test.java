35403df074b79273570e02539bb75c428b57bfb1
Morgan <dalivelyghost@gmail.com>
2020-06-09 15:20:13 -0700

    Fixed the classpath

--- TriviaGUI2/src/test.java
+++ TriviaGUI2/src/test.java
@@ -2,7 +2,7 @@ public class test {
     public static void main(String[] args) {
         QuestionDatabase database = new QuestionDatabase("Questions.csv");
         Maze maze = new Maze(database);
-        database.dropTable();
         new TriviaGUI(maze);
+        database.dropTable();
     }
 }
